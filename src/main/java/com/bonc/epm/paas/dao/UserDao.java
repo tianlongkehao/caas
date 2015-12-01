61c447b145b7597f7f4e599ef59a04fd9e5476ac
Administrator <Administrator@10.0.94.6>
2015-12-01 13:34:13 +0800

    修改配置文件

--- src/main/java/com/bonc/epm/paas/dao/UserDao.java
+++ src/main/java/com/bonc/epm/paas/dao/UserDao.java
@@ -1,9 +1,11 @@
 package com.bonc.epm.paas.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bonc.epm.paas.entity.User;
 
+@Transactional
 public interface UserDao extends CrudRepository<User, Long> {
 	
 	public User findByLoginName(String loginName);
