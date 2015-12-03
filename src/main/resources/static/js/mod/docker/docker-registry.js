952d8d7c7a95e9c041ab49eb515a1146767e401a
peilanxu <1783970412@qq.com>
2015-12-03 17:20:25 +0800

    镜像详细页面

--- src/main/resources/static/js/mod/docker/docker-registry.js
+++ src/main/resources/static/js/mod/docker/docker-registry.js
@@ -9,4 +9,10 @@ $(document).ready(function(){
     });
 
 
+    $(".list_info").click(function(){
+        $(".table_list>.list_info").removeClass("active");
+        $(".table_list").siblings("section").addClass("hide");
+        $(this).addClass("active").parent().siblings("section").eq($(this).index()).removeClass("hide");
+    });
+
 });
