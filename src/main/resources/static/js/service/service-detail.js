717f2f02281237028ed61599e6e15b7c2eeb0df6
peilanxu <1783970412@qq.com>
2015-12-07 17:55:01 +0800

    服务的详细页

---
+++ src/main/resources/static/js/service/service-detail.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+
+    $(".baseInfo>a").click(function(){
+
+        $(".baseInfo>a").removeClass("btn-prim");
+        $(this).addClass("btn-prim");
+    });
+
+    $(".BASE").click(function(){
+
+        $(".contentMain>div:not('.baseInfo')").addClass("hide");
+        $(".containerInfo").removeClass("hide");
+    });
+
+
+    $(".INSTANCES").click(function(){
+
+        $(".contentMain>div:not('.baseInfo')").addClass("hide");
+        $(".containerInstances").removeClass("hide");
+    });
+
+
+    $(".DOMAIN").click(function(){
+
+        $(".contentMain>div:not('.baseInfo')").addClass("hide");
+        $(".bindDomain").removeClass("hide");
+    });
+
+
+    $(".PORTS").click(function(){
+
+        $(".contentMain>div:not('.baseInfo')").addClass("hide");
+        $(".portMapping").removeClass("hide");
+    });
+
+
+    $(".MONITOR").click(function(){
+
+        $(".contentMain>div:not('.baseInfo')").addClass("hide");
+        $(".monitorInfo").removeClass("hide");
+    });
+
+
+    $(".LOG").click(function(){
+
+        $(".contentMain>div:not('.baseInfo')").addClass("hide");
+        $(".containerLog").removeClass("hide");
+    });
+
+
+    $(".EVENT").click(function(){
+
+        $(".contentMain>div:not('.baseInfo')").addClass("hide");
+        $(".containerEvent").removeClass("hide");
+    });
+
+
+    $( "#datePicker" ).datepicker();
+
+});
