58ba962b47800b85029446dfe7fdb2826dbab830
peilanxu <1783970412@qq.com>
2015-12-15 18:05:03 +0800

    增加用户中心页面

---
+++ src/main/resources/static/js/user/user.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $(".Record").click(function(){
+
+        $(".Record").removeClass("active");
+        $(this).addClass("active");
+
+        $(".tab_wrap").addClass("hide");
+        $("#"+$(this).attr("id")+"_wrap").removeClass("hide");
+
+    });
+
+});
