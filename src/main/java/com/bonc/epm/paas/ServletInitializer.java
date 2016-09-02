de746add548fa75b0238e8a9c18e4d58464d6443
王珂 <wangke@bonc.com.cn>
2016-09-02 10:29:59 +0800

    *:重构系统入口类

--- src/main/java/com/bonc/epm/paas/ServletInitializer.java
+++ src/main/java/com/bonc/epm/paas/ServletInitializer.java
@@ -3,6 +3,14 @@ package com.bonc.epm.paas;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
+/**
+ * 
+ * ServletInitializer〉
+ * @author ke_wang
+ * @version 2016年9月2日
+ * @see ServletInitializer
+ * @since
+ */
 public class ServletInitializer extends SpringBootServletInitializer {  
 	  
     @Override  
