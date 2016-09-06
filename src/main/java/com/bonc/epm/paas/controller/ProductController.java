afbea5ea033b79f61ba1db88c7210a70ac07dc77
zhoutao <zhoutao@bonc.com.cn>
2016-09-06 18:00:01 +0800

    * ： 代码优化和调整

--- src/main/java/com/bonc/epm/paas/controller/ProductController.java
+++ src/main/java/com/bonc/epm/paas/controller/ProductController.java
@@ -5,7 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+/**
+ * 
+ * 帮助文档
+ * @author zhoutao
+ * @version 2016年9月6日
+ * @see ProductController
+ * @since
+ */
 @Controller
 @RequestMapping(value = "/product")
 public class ProductController {
@@ -13,14 +20,12 @@ public class ProductController {
 	/**
 	 * 帮助文档
 	 * 
-	 * @param model
-	 * @return
+	 * @param model 添加返回页面的数据
+	 * @return String
 	 */
-	@RequestMapping(value = "/help", method = RequestMethod.GET)
+    @RequestMapping(value = "/help", method = RequestMethod.GET)
 	public String index(Model model) {
-		model.addAttribute("menu_flag", "product");
-		return "product/help.jsp";
-	}
-
-	
+        model.addAttribute("menu_flag", "product");
+        return "product/help.jsp";
+    }
 }
