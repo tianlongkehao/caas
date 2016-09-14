2e9a2a70c85741656f92c12ccdbe1de49fe1bf06
王珂 <wangke@bonc.com.cn>
2016-09-14 18:36:03 +0800

    *：修改cas单点登录

--- src/main/java/com/bonc/epm/paas/sso/filter/SSOFilter.java
+++ src/main/java/com/bonc/epm/paas/sso/filter/SSOFilter.java
@@ -1,6 +1,7 @@
 package com.bonc.epm.paas.sso.filter;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -267,25 +268,27 @@ public class SSOFilter implements Filter {
         try {
             Map<String, Object> params = new HashMap<String, Object>();
             Map<String, Object> data = new HashMap<String, Object>();
-            data.put("tenant_id", "\"" + tenantId + "\"");
-            data.put("resource_type_code", "\"Docker\"");
+            data.put("tenant_id","\""+tenantId+"\"");
+            data.put("resource_type_code","\"Docker\"");
             params.put("param", data);
             String rtnStr = WebClientUtil.doGet(resManUrl, params);
 			// 解析返回资源数据
             if (StringUtils.isNotBlank(rtnStr)) {
                 JSONObject jsStr = JSONObject.parseObject(rtnStr);
-                JSONArray jsArrData = (JSONArray) jsStr.get("data");
-                JSONObject jsObj = (JSONObject) jsArrData.get(0);
-                JSONArray jsArrData2 = (JSONArray) jsObj.get("data");
-                JSONObject jsObj2 = (JSONObject) jsArrData2.get(0);
-                JSONArray jsPro = (JSONArray) jsObj2.get("property");
-                JSONObject cpuObject = (JSONObject) jsPro.get(0);
-                JSONObject memObject = (JSONObject) jsPro.get(1);
-	            // 获取CPU和MEM的值
-                openCpu = (String) cpuObject.get("prop_value");
-                openMem = (String) memObject.get("prop_value");
-                LOG.info("能力平台租户已分配资源:{" + "cpu:" + openCpu + ",mem:" + openMem + "}");
-                createResourceQuota(namespace, openCpu, openMem);
+                if ((boolean)jsStr.get("success")) {
+                    JSONArray jsArrData = (JSONArray) jsStr.get("data");
+                    JSONObject jsObj = (JSONObject) jsArrData.get(0);
+                    JSONArray jsArrData2 = (JSONArray) jsObj.get("data");
+                    JSONObject jsObj2 = (JSONObject) jsArrData2.get(0);
+                    JSONArray jsPro = (JSONArray) jsObj2.get("property");
+                    JSONObject cpuObject = (JSONObject) jsPro.get(0);
+                    JSONObject memObject = (JSONObject) jsPro.get(1);
+                    // 获取CPU和MEM的值
+                    openCpu = (String) cpuObject.get("prop_value");
+                    openMem = (String) memObject.get("prop_value");
+                    LOG.info("能力平台租户已分配资源:{" + "cpu:" + openCpu + ",mem:" + openMem + "}");
+                    createResourceQuota(namespace, openCpu, openMem);
+                }
             }
         }
         catch (Exception e) {
