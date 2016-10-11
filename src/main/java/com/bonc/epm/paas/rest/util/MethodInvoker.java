3baf398eca93d98c2d24c94828269dcf6cac584c
longkaixiang <longkaixiang@bonc.com.cn>
2016-10-11 18:04:47 +0800

    log不打到控制台console 2016年10月11日 18:04:43

--- src/main/java/com/bonc/epm/paas/rest/util/MethodInvoker.java
+++ src/main/java/com/bonc/epm/paas/rest/util/MethodInvoker.java
@@ -100,7 +100,7 @@ public class MethodInvoker {
     		response = invocationBuilder.put(entity);
     	}
     	response.bufferEntity();
-    	log.info(url+pathValue+" -X"+get+":"+post+":"+delete+":"+put+"========response:"+response.readEntity(String.class));
+//    	log.info(url+pathValue+" -X"+get+":"+post+":"+delete+":"+put+"========response:"+response.readEntity(String.class));
     	try{
     		return response.readEntity(method.getReturnType());
     	}catch(Exception e){
