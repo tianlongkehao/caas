02aa94a94d502884b9a997cf2a946d6308a44700
jiangliya <jiangliya@bonc.com.cn>
2016-11-11 14:31:17 +0800

    去掉name上的数字

--- src/main/resources/static/js/ci/ci_add.js
+++ src/main/resources/static/js/ci/ci_add.js
@@ -37,14 +37,14 @@ $(document).ready(function () {
     			'<div class="row ant-config">'+
                 	'<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">ant版本</label>'+
-                        '<select id="antVersion-'+count+'" name="antVersion-'+count+'" class="form-control c-project-con" >'+
+                        '<select id="antVersion-'+count+'" name="antVersion" class="form-control c-project-con" >'+
                         	'<option value="default">default</option>'+
                             '<option value="ant">ant</option>'+
                         '</select>'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                        '<label class="c-project-tit">目标</label>'+
-                        '<input id="antTargets-'+count+'" name="antTargets-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="antTargets-'+count+'" name="antTargets" type="text" class="form-control c-project-con" value="">'+
                     '</div>'+
                 '</div>'+
                 '<div class="row ant-config">'+
@@ -53,15 +53,15 @@ $(document).ready(function () {
                 '<div class="row ant-higherCon">'+
                 	'<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">构建文件</label>'+
-                        '<input id="antBuildFileLocation-'+count+'" name="antBuildFileLocation-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="antBuildFileLocation-'+count+'" name="antBuildFileLocation" type="text" class="form-control c-project-con" value="">'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">性能</label>'+
-                        '<input id="antProperties-'+count+'" name="antProperties-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="antProperties-'+count+'" name="antProperties" type="text" class="form-control c-project-con" value="">'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">java选项</label>'+
-                        '<input id="antJavaOpts-'+count+'" name="antJavaOpts-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="antJavaOpts-'+count+'" name="antJavaOpts" type="text" class="form-control c-project-con" value="">'+
                     '</div></div></div></div></div></div>';
     	return antHtml;
     }
@@ -85,49 +85,49 @@ $(document).ready(function () {
     			'<div class="row">'+
                 	'<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">maven版本</label>'+
-                        '<select id="mavenVersion-'+count+'" name="mavenVersion-'+count+'" class="form-control c-project-con" >'+
+                        '<select id="mavenVersion-'+count+'" name="mavenVersion" class="form-control c-project-con" >'+
                         	'<option value="default">default</option>'+
                             '<option value="maven">maven</option>'+
                         '</select>'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">目标</label>'+
-                        '<input id="mavenGoals-'+count+'" name="mavenGoals-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="mavenGoals-'+count+'" name="mavenGoals" type="text" class="form-control c-project-con" value="">'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">POM</label>'+
-                        '<input id="pomLocation-'+count+'" name="pomLocation-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="pomLocation-'+count+'" name="pomLocation" type="text" class="form-control c-project-con" value="">'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">属性</label>'+
-                        '<input id="mavenProperty-'+count+'" name="mavenProperty-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="mavenProperty-'+count+'" name="mavenProperty" type="text" class="form-control c-project-con" value="">'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">JVM选项</label>'+
-                        '<input id="mavenJVMOptions-'+count+'" name="mavenJVMOptions-'+count+'" type="text" class="form-control c-project-con" value="">'+
+                        '<input id="mavenJVMOptions-'+count+'" name="mavenJVMOptions" type="text" class="form-control c-project-con" value="">'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<div style="line-height:34px">'+
                             '<label class="c-project-tit" title="使用私人maven存储库">使用私人maven存储库</label>'+
-                            '<input type="checkbox" id = "isUserPrivateRegistry-'+count+'" name = "isUserPrivateRegistry-'+count+'" value = "0">'+
+                            '<input type="checkbox" id = "isUserPrivateRegistry" name = "isUserPrivateRegistry-'+count+'" value = "0">'+
                         '</div>'+
                     '</div>'+
                      '<div class="form-group col-md-12">'+
                         '<div style="line-height:34px">'+
                             '<label class="c-project-tit">注入建立变量</label>'+
-                            '<input type="checkbox"  id="injectBuildVariables-'+count+'" name="injectBuildVariables-'+count+'" value="0">'+
+                            '<input type="checkbox"  id="injectBuildVariables" name="injectBuildVariables-'+count+'" value="0">'+
                         '</div>'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">配置文件</label>'+
-                        '<select id="mavenSetFile-'+count+'" name="mavenSetFile-'+count+'" class="form-control c-project-con" >'+
+                        '<select id="mavenSetFile-'+count+'" name="mavenSetFile" class="form-control c-project-con" >'+
                         	'<option value="use default maven setting">use default maven setting</option>'+
                             '<option value="settings file in filesystem">settings file in filesystem</option>'+
                         '</select>'+
                     '</div>'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">全局配置文件</label>'+
-                        '<select id="mavenGlobalSetFile-'+count+'" name="mavenGlobalSetFile-'+count+'" class="form-control c-project-con" >'+
+                        '<select id="mavenGlobalSetFile-'+count+'" name="mavenGlobalSetFile" class="form-control c-project-con" >'+
                         	'<option value="use default maven global setting">use default maven global setting</option>'+
                             '<option value="global settings file in filesystem">global settings file in filesystem</option>'+
                         '</select>'+
@@ -156,7 +156,7 @@ $(document).ready(function () {
     			'<div class="row">'+
                     '<div class="form-group col-md-12">'+
                         '<label class="c-project-tit">命令</label>'+
-                        '<textarea id="executeShell-'+count+'" name="executeShell-'+count+'" class="form-control c-project-con" type="text" row="5"></textarea>'+
+                        '<textarea id="executeShell-'+count+'" name="executeShell" class="form-control c-project-con" type="text" row="5"></textarea>'+
                     '</div></div></div></div></div></div>';
     	return shellHtml;
     }
