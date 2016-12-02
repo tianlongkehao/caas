2a240cf53f9747ba1c3c4bae8e2896f7b89c8e62
王珂 <wangke@bonc.com.cn>
2016-12-02 10:59:10 +0800

    *:修改创建服务名字 校验

--- src/main/resources/static/js/service/service_create.js
+++ src/main/resources/static/js/service/service_create.js
@@ -22,13 +22,13 @@ $(document).ready(function(){
 	      $('#serviceName').focus();
 	      return;
 	    }
-	    if(name.search(/^[a-z][a-z0-9-]*$/) === -1){
+	    if(name.search(/[a-z]([-a-z0-9]*[a-z0-9])?/) === -1){
 	      layer.tips('服务名称只能由小写字母、数字及横线组成，且首字母不能为数字及横线。','#serviceName',{tips: [1, '#3595CC'],time: 3000});
 	      $('#serviceName').focus();
 	      return;
 	    }
-	    if(name.length > 64 || name.length < 3){
-	      layer.tips('服务名称为3~64个字符','#serviceName',{tips: [1, '#3595CC'],time: 3000});
+	    if(name.length > 24 || name.length < 1){
+	      layer.tips('服务名称为1~24个字符','#serviceName',{tips: [1, '#3595CC'],time: 3000});
 	      $('#serviceName').focus();
 	      return;
 	    }
