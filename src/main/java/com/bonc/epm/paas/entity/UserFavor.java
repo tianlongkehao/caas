b26a5505a20c13f5ad497ee721d3a4b1de529b24
longkaixiang <longkaixiang@bonc.com.cn>
2016-12-30 14:31:08 +0800

    增加用户偏好userfavor类 2016年12月30日 14:31:06

---
+++ src/main/java/com/bonc/epm/paas/entity/UserFavor.java
@@ -0,0 +1,37 @@
+package com.bonc.epm.paas.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserFavor {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;// 索引
+	//用户id
+	private long userId;
+	//监控设置
+	private String monitor;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getMonitor() {
+		return monitor;
+	}
+	public void setMonitor(String monitor) {
+		this.monitor = monitor;
+	}
+}
