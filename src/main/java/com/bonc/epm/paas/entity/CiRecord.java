eab62d4a053519801124692b180f05a71ba9bbb0
longkaixiang <longkaixiang@bonc.com.cn>
2017-02-10 15:43:32 +0800

    增加构建信息的字段 2017年2月10日 15:43:29

--- src/main/java/com/bonc/epm/paas/entity/CiRecord.java
+++ src/main/java/com/bonc/epm/paas/entity/CiRecord.java
@@ -13,8 +13,9 @@ import javax.persistence.Lob;
 import com.alibaba.fastjson.annotation.JSONField;
 
 /**
- * 
+ *
  * 构建记录信息实体类
+ *
  * @author update
  * @version 2016年8月31日
  * @see CiRecord
@@ -22,115 +23,140 @@ import com.alibaba.fastjson.annotation.JSONField;
  */
 @Entity
 public class CiRecord {
-    
-    /**
-     * 主键
-     */
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private long id;
-	
+
+	/**
+	 * 主键
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
 	/**
 	 * 构建id
 	 */
-    private long ciId;
-    
+	private long ciId;
+
+	/**
+	 * 构建名称
+	 */
+	private String ciName;
+
 	/**
 	 * 构建版本
 	 */
-    private String ciVersion;
-    
+	private String ciVersion;
+
+	/**
+	 * 构建镜像id
+	 */
+	private long imageId;
+
 	/**
 	 * 构建时间
 	 */
-    @JSONField(format = "yyyy-MM-dd HH:mm:ss")
+	@JSONField(format = "yyyy-MM-dd HH:mm:ss")
 	private Date constructDate;
-    
+
 	/**
 	 * 构建时长
 	 */
-    private long constructTime;
-    
+	private long constructTime;
+
 	/**
 	 * 构建结果：1成功2失败3构建中
 	 */
-    private Integer constructResult;
-    
-    /**
-     * 代码构建的seqNo
-     */
-    private Integer executionId;
-	
+	private Integer constructResult;
+
+	/**
+	 * 代码构建的seqNo
+	 */
+	private Integer executionId;
+
 	/**
 	 * 构建日志
 	 */
-    @Lob
+	@Lob
 	@Basic(fetch = FetchType.LAZY)
 	private String logPrint;
-	
-    public long getId() {
-        return id;
-    }
-    
-    public void setId(long id) {
-        this.id = id;
-    }
-    
-    public long getCiId() {
-        return ciId;
-    }
-    
-    public void setCiId(long ciId) {
-        this.ciId = ciId;
-    }
-    
-    public String getCiVersion() {
-        return ciVersion;
-    }
-    
-    public void setCiVersion(String ciVersion) {
-        this.ciVersion = ciVersion;
-    }
-    
-    public Date getConstructDate() {
-        return constructDate;
-    }
-    
-    public void setConstructDate(Date constructDate) {
-        this.constructDate = constructDate;
-    }
-    
-    public long getConstructTime() {
-        return constructTime;
-    }
-    
-    public void setConstructTime(long constructTime) {
-        this.constructTime = constructTime;
-    }
-    
-    public Integer getConstructResult() {
-        return constructResult;
-    }
-    
-    public void setConstructResult(Integer constructResult) {
-        this.constructResult = constructResult;
-    }
-	
-    public Integer getExecutionId() {
-        return executionId;
-    }
 
-    public void setExecutionId(Integer executionId) {
-        this.executionId = executionId;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public String getLogPrint() {
-        return logPrint;
-    }
-    
-    public void setLogPrint(String logPrint) {
-        this.logPrint = logPrint;
-    }
-	
-	
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public long getCiId() {
+		return ciId;
+	}
+
+	public void setCiId(long ciId) {
+		this.ciId = ciId;
+	}
+
+	public String getCiVersion() {
+		return ciVersion;
+	}
+
+	public void setCiVersion(String ciVersion) {
+		this.ciVersion = ciVersion;
+	}
+
+	public Date getConstructDate() {
+		return constructDate;
+	}
+
+	public void setConstructDate(Date constructDate) {
+		this.constructDate = constructDate;
+	}
+
+	public long getConstructTime() {
+		return constructTime;
+	}
+
+	public void setConstructTime(long constructTime) {
+		this.constructTime = constructTime;
+	}
+
+	public Integer getConstructResult() {
+		return constructResult;
+	}
+
+	public void setConstructResult(Integer constructResult) {
+		this.constructResult = constructResult;
+	}
+
+	public Integer getExecutionId() {
+		return executionId;
+	}
+
+	public void setExecutionId(Integer executionId) {
+		this.executionId = executionId;
+	}
+
+	public String getLogPrint() {
+		return logPrint;
+	}
+
+	public void setLogPrint(String logPrint) {
+		this.logPrint = logPrint;
+	}
+
+	public String getCiName() {
+		return ciName;
+	}
+
+	public void setCiName(String ciName) {
+		this.ciName = ciName;
+	}
+
+	public long getImageId() {
+		return imageId;
+	}
+
+	public void setImageId(long imageId) {
+		this.imageId = imageId;
+	}
+
 }
