6c421dce995149f4211463f9be53b878556d844d
longkaixiang <longkaixiang@bonc.com.cn>
2017-05-27 17:40:26 +0800

    修改监控 是数据显示 2017年5月27日 17:40:23

--- src/main/java/com/bonc/epm/paas/constant/MonitorConstant.java
+++ src/main/java/com/bonc/epm/paas/constant/MonitorConstant.java
@@ -11,16 +11,16 @@ public class MonitorConstant {
     public static final String NEGATIVE_VALUE_U = " non_negative_derivative(max(value),1u) ";
     public static final String NEGATIVE_VALUE_S = " non_negative_derivative(max(value),1s) ";
 
-    public static final String MEMORY_LIMIT = " \"memory/limit_bytes_gauge\" ";
-    public static final String MEMORY_USAGE = " \"memory/usage_bytes_gauge\" ";
-    public static final String MEMORY_WORKING_SET = " \"memory/working_set_bytes_gauge\" ";
+    public static final String MEMORY_LIMIT = " \"memory/limit\" ";
+    public static final String MEMORY_USAGE = " \"memory/usage\" ";
+    public static final String MEMORY_WORKING_SET = " \"memory/working_set\" ";
 
-    public static final String CPU_LIMIT = " \"cpu/limit_gauge\" ";
-    public static final String CPU_USAGE = " \"cpu/usage_ns_cumulative\" ";
+    public static final String CPU_LIMIT = " \"cpu/limit\" ";
+    public static final String CPU_USAGE = " \"cpu/usage\" ";
 
-    public static final String FILE_LIMIT = " \"filesystem/limit_bytes_gauge\" ";
-    public static final String FILE_USAGE = " \"filesystem/usage_bytes_gauge\" ";
+    public static final String FILE_LIMIT = " \"filesystem/limit\" ";
+    public static final String FILE_USAGE = " \"filesystem/usage\" ";
 
-    public static final String NET_TX = " \"network/tx_bytes_cumulative\" ";
-    public static final String NET_RX = " \"network/rx_bytes_cumulative\" ";
+    public static final String NET_TX = " \"network/tx\" ";
+    public static final String NET_RX = " \"network/rx\" ";
 }
