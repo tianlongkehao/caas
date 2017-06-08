f86976ab9b941837bcbc68fc220269bf129a978a
longkaixiang <longkaixiang@bonc.com.cn>
2017-06-08 16:28:14 +0800

    修改资源的换算 2017年6月8日 16:28:12

--- src/main/java/com/bonc/epm/paas/util/ConvertUtil.java
+++ src/main/java/com/bonc/epm/paas/util/ConvertUtil.java
@@ -71,4 +71,54 @@ public class ConvertUtil {
 
 		return ret;
 	}
+
+	/**
+	 * convertMemory:转换内存值字符串为无单位数值. <br/>
+	 *
+	 * @param memory
+	 * @return double
+	 */
+	public static double ParseMemory(String memory) {
+		if (StringUtils.isEmpty(memory)) {
+			return 0d;
+		}
+		memory = memory.replaceAll("i", "");
+		double ret;
+		if (memory.endsWith("n")) {
+			ret = Double.parseDouble(memory.replace("n", "")) * Math.pow(10, -9);
+		} else if (memory.endsWith("u")) {
+			ret = Double.parseDouble(memory.replace("u", "")) * Math.pow(10, -6);
+		} else if (memory.endsWith("m")) {
+			ret = Double.parseDouble(memory.replace("m", "")) * Math.pow(10, -3);
+		} else if (memory.endsWith("k")) {
+			ret = Double.parseDouble(memory.replace("k", "")) * Math.pow(10, 3);
+		} else if (memory.endsWith("K")) {
+			ret = Double.parseDouble(memory.replace("K", "")) * Math.pow(10, 3);
+		} else if (memory.endsWith("M")) {
+			ret = Double.parseDouble(memory.replace("M", "")) * Math.pow(10, 6);
+		} else if (memory.endsWith("G")) {
+			ret = Double.parseDouble(memory.replace("G", "")) * Math.pow(10, 9);
+		} else if (memory.endsWith("T")) {
+			ret = Double.parseDouble(memory.replace("T", "")) * Math.pow(10, 12);
+		} else if (memory.endsWith("P")) {
+			ret = Double.parseDouble(memory.replace("P", "")) * Math.pow(10, 15);
+		} else if (memory.endsWith("E")) {
+			ret = Double.parseDouble(memory.replace("E", "")) * Math.pow(10, 18);
+		} else if (memory.endsWith("Ki")) {
+			ret = Double.parseDouble(memory.replace("Ki", "")) * Math.pow(2, 10);
+		} else if (memory.endsWith("Mi")) {
+			ret = Double.parseDouble(memory.replace("Mi", "")) * Math.pow(2, 20);
+		} else if (memory.endsWith("Gi")) {
+			ret = Double.parseDouble(memory.replace("Gi", "")) * Math.pow(2, 30);
+		} else if (memory.endsWith("Ti")) {
+			ret = Double.parseDouble(memory.replace("Ti", "")) * Math.pow(2, 40);
+		} else if (memory.endsWith("Pi")) {
+			ret = Double.parseDouble(memory.replace("Pi", "")) * Math.pow(2, 50);
+		} else if (memory.endsWith("Ei")) {
+			ret = Double.parseDouble(memory.replace("Ei", "")) * Math.pow(2, 60);
+		} else {
+			ret = Double.parseDouble(memory);
+		}
+		return ret;
+	}
 }
